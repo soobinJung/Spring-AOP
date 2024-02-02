3f1338c572b5d948d22b3dd83a753defa99f3863
soobinJung <soobin84786861@gmail.com>
2024-02-03 00:19:15 +0900

    @Pointcut 을 사용한 Aspect

---
+++ aop/src/main/java/com/aop/common/AspectV2.java
@@ -0,0 +1,39 @@
+package com.aop.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * @Pointcut 을 사용한 Aspect
+ *
+ * - 메서드 이름과 파라메터를 합쳐서 포인트컷 시그니쳐라 한다.
+ * - 포인트컷을 별도의 메서드로 분리하고, @Pointcut 을 사용하여 재사용한다.
+ * - 메서드의 반환 타입은 void 이고 코드 내용은 비어있는 메서드로 선언한다.
+ */
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+    /**
+     * PointCut 시그니쳐
+     */
+    @Pointcut("execution(* com.aop..*.*(..))")
+    private void allOrder(){}
+
+    @Around("allOrder()")
+    public Object doLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        /** 시작 시간 **/
+        log.info("Start - " + joinPoint.getSignature().getDeclaringTypeName() + " / " + joinPoint.getSignature().getName());
+
+        /** 실제 타겟이 호출되는 시점 **/
+        Object result = joinPoint.proceed();
+
+        /** 종료 시간 **/
+        log.info("End - " + joinPoint.getSignature().getDeclaringTypeName() + " / " + joinPoint.getSignature().getName());
+        return result;
+    }
+}
---
+++ aop/src/test/java/com/aop/AopV2Test.java
@@ -0,0 +1,35 @@
+package com.aop;
+
+import com.aop.common.AspectV1;
+import com.aop.common.AspectV2;
+import com.aop.order.OrderRepository;
+import com.aop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(AspectV2.class)
+@Slf4j
+@SpringBootTest
+public class AopV2Test {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    /**
+     * AOP Proxy 가 적용 되었는지 확인
+     */
+    @Test
+    public void aopInfo(){
+        log.info("Proxy 가 적용 되었는지 체크 : " + AopUtils.isAopProxy(orderService));
+        log.info("Proxy 가 적용 되었는지 체크 : " + AopUtils.isAopProxy(orderRepository));
+
+        orderService.save("item1");
+    }
+}
